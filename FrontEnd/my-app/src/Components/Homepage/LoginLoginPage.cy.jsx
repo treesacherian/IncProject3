b09584c51eede569fa592253d351061b00d37299
ZMichaelaK <korn_miku@hotmail.com>
2024-04-19 10:19:48 +0100

    more cy tests

---
+++ FrontEnd/my-app/src/Components/Homepage/LoginLoginPage.cy.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginPage from "./Login";
+import { BrowserRouter } from "react-router-dom";
+describe("<LoginPage />", () => {
+  it("renders", () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(
+      <BrowserRouter>
+        <LoginPage />
+      </BrowserRouter>
+    );
+  });
+});
