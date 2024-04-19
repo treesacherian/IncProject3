14e76ce2694dabd6fac6e4e58fa86cadc7801197
treesacherian <treesa.cherian@gmail.com>
2024-04-19 10:22:55 +0100

    merging

--- FrontEnd/my-app/src/Components/Basket/BasketDisplayBasketsDisplay.cy.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import BasketsDisplay from './BasketDisplay'
-
-describe('<BasketsDisplay />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<BasketsDisplay />)
-  })
-})
