de10fae32e23e381204ce9dc04d8b5d58a2a8c04
ZMichaelaK <korn_miku@hotmail.com>
2024-04-22 11:10:42 +0100

    jenkins listening test

--- FrontEnd/my-app/src/Components/Homepage/Home.jsx
+++ FrontEnd/my-app/src/Components/Homepage/Home.jsx
@@ -6,7 +6,7 @@ import EditItem from '../Item/EditItem';
 function Home() {
   return (
     <div style={{ paddingTop: "30px" }}>
-      <h1>"Aldi.. the better Lidl"</h1>
+      <span style="color: red;">Aldi.. the better Lidl</span>
     </div>
   );
 }
